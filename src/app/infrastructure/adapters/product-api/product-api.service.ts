48c412a38ecd740d37fb1918b7c6a4426e1e2958
InnitAlejandro <alejose1710@gmail.com>
2024-03-13 19:56:02 -0500

    feat(product-service): init product infrastructure

---
+++ src/app/infrastructure/adapters/product-api/product-api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { ProductGateway } from '../../../domain/models/product/gateway/product.gateway';
+import { Product } from '../../../domain/models/product/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductApiService extends ProductGateway {
+  private url = 'https://api.escuelajs.co/api/v1/products'
+
+  constructor(private http: HttpClient) {super();}
+
+  getByID(id: string): Observable<Product>{
+    return this.http.get<Product>(this.url+id)
+  }
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Array<Product>>(this.url);
+  }
+}
