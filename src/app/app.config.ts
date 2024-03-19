65ff37df570c58a33b6c31315ed92afc4c78e144
InnitAlejandro <alejose1710@gmail.com>
2024-03-18 19:24:45 -0500

    feat(routing): init routing and app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ProductGateway } from './domain/models/product/gateway/product.gateway';
+import { ProductApiService } from './infrastructure/adapters/product-api/product-api.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    { provide: ProductGateway, useClass: ProductApiService },
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './ui/pages/home/home.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent}
+];
