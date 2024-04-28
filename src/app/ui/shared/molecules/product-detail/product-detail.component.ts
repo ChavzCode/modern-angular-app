2a3bb9bb806c59d2de83e50e77ec807a3f6308da
InnitAlejandro <alejose1710@gmail.com>
2024-04-27 19:23:00 -0500

    feat(product-detail): create product details visualization ui

---
+++ src/app/ui/shared/molecules/product-detail/product-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+import { Product } from '../../../../domain/models/product/product';
+import { initProduct } from '../../../core/constants/product.constants';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css'
+})
+export class ProductDetailComponent {
+  @Input() product: Product = initProduct;
+}
